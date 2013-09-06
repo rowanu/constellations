bd62aede0fcbe29549e3e1729c00d2f563780878
rowan <rowanu@gmail.com>
2013-09-06 19:55:27 -0400

    Add GitHub User resouce service

--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,8 @@
+/*jslint indent: 2 */
+/*globals angular: false */
 'use strict';
 
-/* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('ghcApp.services', []).
-  value('version', '0.1');
+angular.module('ghcApp.services', [])
+  .factory('User', function ($resource) {
+    return $resource('https://api.github.com/users/:username');
+  });
