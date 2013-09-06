63401d316006f17a57a5d3a1e9441429fa4ad239
rowan <rowanu@gmail.com>
2013-09-06 19:54:25 -0400

    Added User controller and partial

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,16 @@
+/*jslint indent: 2 */
+/*globals angular: false */
 'use strict';
 
-/* Controllers */
-
-angular.module('ghcApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
+angular.module('ghcApp.controllers', [])
+  .controller('UserCtrl', [function ($scope, User) {
+    $scope.submit = function () {
+      User.get({username: $scope.username}, function (user) {
+        // console.log(user);
+        $scope.avatar_url = user.avatar_url;
+        // $scope.$emit('user:update', user);
+      }, function (response) {
+        $scope.avatar_url = 'http://octodex.github.com/images/daftpunktocat-thomas.gif';
+      });
+    };
   }]);
