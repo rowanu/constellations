cc4b199a95a0d657f9f3e041b78c80a48ed914ba
rowan <rowanu@gmail.com>
2013-09-07 15:32:03 -0400

    Add loading image to user controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,16 +2,20 @@
 /*globals angular: false */
 'use strict';
 
+var avatar404 = '/img/404_octocat.png',
+  avatarLoading = '/img/spinner.gif';
+
 angular.module('ghcApp.controllers', [])
   .controller('MainCtrl', [function ($scope) {
   }])
   .controller('UserCtrl', ['$scope', 'User', function ($scope, User) {
     $scope.submit = function () {
+      $scope.avatarUrl = avatarLoading;
       User.get({username: $scope.username}, function success(user) {
         $scope.avatarUrl = user.avatar_url;
         $scope.$emit('user:update', user);
       }, function error(response) {
-        $scope.avatarUrl = '/img/404_octocat.png';
+        $scope.avatarUrl = avatar404;
       });
     };
   }]);
