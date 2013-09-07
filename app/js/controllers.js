850ae4f71c0c63676e7af4bf32717d8935e4aa06
rowan <rowanu@gmail.com>
2013-09-07 15:28:43 -0400

    Add 404 image for username search

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,12 +7,11 @@ angular.module('ghcApp.controllers', [])
   }])
   .controller('UserCtrl', ['$scope', 'User', function ($scope, User) {
     $scope.submit = function () {
-      User.get({username: $scope.username}, function (user) {
-        // console.log(user);
-        $scope.avatar_url = user.avatar_url;
+      User.get({username: $scope.username}, function success(user) {
+        $scope.avatarUrl = user.avatar_url;
         $scope.$emit('user:update', user);
-      }, function (response) {
-        $scope.avatar_url = 'http://octodex.github.com/images/daftpunktocat-thomas.gif';
+      }, function error(response) {
+        $scope.avatarUrl = '/img/404_octocat.png';
       });
     };
   }]);
