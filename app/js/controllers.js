f1604a93de6abb97e6a6c87288b11193312afb21
rowan <rowanu@gmail.com>
2013-09-07 15:15:58 -0400

    Add test for event emitter

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,7 +10,7 @@ angular.module('ghcApp.controllers', [])
       User.get({username: $scope.username}, function (user) {
         // console.log(user);
         $scope.avatar_url = user.avatar_url;
-        // $scope.$emit('user:update', user);
+        $scope.$emit('user:update', user);
       }, function (response) {
         $scope.avatar_url = 'http://octodex.github.com/images/daftpunktocat-thomas.gif';
       });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,14 +3,29 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('ghcApp.controllers'));
+  var scope, UserCtrl, $httpBackend;
+  var user = {
+    login: 'rowanu',
+    avatar_url: 'OHAI'
+  };
 
+  beforeEach(module('ghcApp.controllers', 'ghcApp.services'));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  describe('UserCtrl', function () {
+    beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+      scope = $rootScope.$new();
+      UserCtrl = $controller('UserCtrl', {$scope: scope});
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('https://api.github.com/users/rowanu')
+        .respond(user);
+    }));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    it('should emit an update on update', inject(function() {
+      spyOn(scope, '$emit');
+      scope.username = 'rowanu';
+      scope.submit();
+      $httpBackend.flush();
+      expect(scope.$emit).toHaveBeenCalled();
+    }));
+  })
 });
