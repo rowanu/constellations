29660bc435bebd4d09a1fe4e34cffedeb67c638c
rowan <rowanu@gmail.com>
2013-09-08 20:11:55 -0400

    Remove unused Constellation service code. Include loading and 404 images.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,9 @@
 /*globals angular: false */
 'use strict';
 
+var avatar404 = '/img/404_octocat.png',
+  avatarLoading = '/img/spinner.gif';
+
 angular.module('constellationsApp.controllers', [])
   .controller('MainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.$on('username:submit', function (e, username) {
@@ -10,11 +13,14 @@ angular.module('constellationsApp.controllers', [])
   }])
   .controller('UserCtrl', ['$scope', 'GitHub', function ($scope, GitHub) {
     $scope.$on("username", function (e, username) {
+      $scope.user = {avatar_url: avatarLoading, html_url: '/'};
       GitHub.user.get({username: username}, function success(user) {
+        console.log(username + ": Got GitHub user");
         console.log(user); // TODO: Testing
         $scope.user = user;
       }, function error() {
-        console.log(username + ": GitHub user not found.");
+        console.log(username + ": GitHub user not found");
+        $scope.user = {avatar_url: avatar404, html_url: '/'};
       });
     });
   }])
--- app/js/services.js
+++ app/js/services.js
@@ -2,39 +2,7 @@
 /*globals angular: false */
 'use strict';
 
-var avatar404 = '/img/404_octocat.png',
-  avatarLoading = '/img/spinner.gif';
-
 angular.module('constellationsApp.services', ['ngResource'])
-  .factory('Constellation', function (GitHub) {
-    return {
-      getUser: function (username) {
-        var self = this;
-        console.log(username + ": Getting GitHub user");
-        self.user = {avatar_url: avatarLoading, html_url: ''};
-        GitHub.user.get({username: username}, function success(user) {
-          console.log(username + ": Found GitHub user");
-          self.user = user;
-        }, function error(response) {
-          console.log(username + ": No GitHub user found");
-          self.user = {avatar_url: avatar404, html_url: ''};
-        });
-      },
-      getFollowing: function (username) {
-        var self = this;
-        console.log(username + ": Getting GitHub following");
-        GitHub.following.get({username: username}, function success(following) {
-          console.log(username + ": Found Github following");
-          angular.forEach(following, function (f) {
-            console.log(f.login + ': ' + f.html_url);
-          });
-        }, function error(response) {
-          console.log(username + ": No Github following found");
-        });
-      },
-      user: {}
-    };
-  })
   .factory('GitHub', function ($resource) {
     return {
       user: $resource('https://api.github.com/users/:username'),
