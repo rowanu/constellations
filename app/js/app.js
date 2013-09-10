f4e8edfc6c9b90da5978b0b84e99d78f5b10511d
rowan <rowanu@gmail.com>
2013-09-10 17:16:42 -0400

    Upgrade ngResource to 1.2.0rc2 (to follow angular upgrade). Include ngResource (required by Angular 1.2.0+)

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,12 @@
+/*globals angular: false */
 'use strict';
-
-// Declare app level module which depends on filters, and services
-angular.module('constellationsApp', ['constellationsApp.filters', 'constellationsApp.services', 'constellationsApp.directives', 'constellationsApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
+angular.module('constellationsApp', [
+  'ngRoute',
+  'constellationsApp.filters',
+  'constellationsApp.services',
+  'constellationsApp.directives',
+  'constellationsApp.controllers'
+]).config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
