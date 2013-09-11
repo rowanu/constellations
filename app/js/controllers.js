fbcc711e8f390f1a87c0a0f5e9ed62e8dd6f58d3
rowan <rowanu@gmail.com>
2013-09-10 21:19:15 -0400

    Move StarredCtrl to GitHub service.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,42 +2,20 @@
 /*globals angular: false */
 'use strict';
 
-var avatar404 = '/img/404_octocat.png',
-  avatarLoading = '/img/spinner.gif';
-
 angular.module('constellationsApp.controllers', [])
-  .controller('MainCtrl', ['$scope', 'Constellation', function ($scope, Constellation) {
+  .controller('MainCtrl', ['$scope', '$q', 'Constellation', function ($scope, $q, Constellation) {
     // Share the username with child controllers.
     $scope.$on('username:submit', function (e, username) {
-      // $scope.$broadcast('username:update', username);
-      $scope.following = Constellation.getFollowing(username);
-      console.log($scope.following);
-    });
-  }])
-  .controller('UserCtrl', ['$scope', 'GitHub', function ($scope, GitHub) {
-    $scope.$on("username:update", function (e, username) {
-      $scope.user = {avatar_url: avatarLoading, html_url: '/'};
-      GitHub.user.get({username: username}, function success(user) {
-        console.log(username + ": Got GitHub user");
-        $scope.user = user;
-      }, function error() {
-        console.error(username + ": GitHub user not found");
-        $scope.user = {avatar_url: avatar404, html_url: '/'};
-      });
-    });
-  }])
-  .controller('FollowingCtrl', ['$scope', 'GitHub', function ($scope, GitHub) {
-    $scope.$on("username:update", function (e, username) {
-    });
-  }])
-  .controller('StarredCtrl', ['$scope', 'GitHub', function ($scope, GitHub) {
-    $scope.$on("username:update", function (e, username) {
-      GitHub.starred.get({username: username}, function success(starred) {
-        console.log(username + ": Got GitHub starred");
-        $scope.starred = starred;
-      }, function error() {
-        console.error(username + ": GitHub starred not found");
-        $scope.starred = [];
+      var ready = $q.all([
+        Constellation.getFollowing(username)
+      ]);
+      $scope.user = Constellation.getUser(username);
+
+      ready.then(function success(results) {
+        $scope.following = results[0];
+      }, function error(reason) {
+        console.error(username + ": Not ready " + reason);
+        $scope.following = [];
       });
     });
   }])
--- app/js/services.js
+++ app/js/services.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var PER_PAGE = 100;
+var AVATAR_404 = '/img/404_octocat.png';
 
 angular.module('constellationsApp.services', ['ngResource'])
   .factory('Constellation', ['GitHub', '$q', function (GitHub, $q) {
@@ -11,15 +12,39 @@ angular.module('constellationsApp.services', ['ngResource'])
       getFollowing: function (username) {
         var deferred = $q.defer();
         GitHub.following.get({username: username}, function success(following) {
-          console.log(username + ": Got GitHub following");
-          this.following = following;
+          console.log(username + ': Got GitHub following ' + following.length);
+          // TODO: Store following for later reference?
+          // this.following = following;
           deferred.resolve(following);
-        }, function error(err) {
-          console.error(username + ": GitHub following not found");
-          deferred.reject(err);
+        }, function error(reason) {
+          console.error(username + ': GitHub following not found');
+          deferred.reject(reason);
         });
         return deferred.promise;
       },
+      getUser: function (username) {
+        var deferred = $q.defer();
+        GitHub.user.get({username: username}, function success(user) {
+          console.log(username + ': Got GitHub user');
+          deferred.resolve(user);
+        }, function error(reason) {
+          console.error(username + ': GitHub user not found');
+          deferred.resolve({avatar_url: AVATAR_404, html_url: '/'});
+        });
+        return deferred.promise;
+      },
+      getStarred: function (username) {
+        var deferred = $q.defer();
+        GitHub.starred.get({username: username}, function success(starred) {
+          console.log(username + ': Got GitHub starred ' + starred.length);
+          deferred.resolve(starred);
+        }, function error() {
+          console.error(username + ': GitHub starred not found');
+          deferred.resolve([]);
+        });
+        return deferred.promise;
+      },
+      // TODO: Work for all types eg. starred.
       getAllStarred: function (username, successCallback, errorCallback) {
         var allStarred = [],
           page = 1, // GitHub API paging is 1-based.
@@ -27,19 +52,19 @@ angular.module('constellationsApp.services', ['ngResource'])
           error;
 
         error = function (data) {
-          console.error(username + ": Problem with starred");
+          console.error(username + ': Problem with starred');
           errorCallback(data);
         };
 
         success = function (starred) {
-          console.log(username + ": Got starred page " + page);
+          console.log(username + ': Got starred page ' + page);
           allStarred = allStarred.concat(starred);
           if (starred.length === PER_PAGE) {
             page += 1;
-            console.log(username + ": Getting starred page " + page);
+            console.log(username + ': Getting starred page ' + page);
             GitHub.starred.get({username: username, page: page}, success, error);
           } else {
-            console.log(username + "Got all starred");
+            console.log(username + 'Got all starred');
             successCallback(allStarred);
           }
         };
